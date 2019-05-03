549af5b866ccf8ec1727e4fd2bcc00178ec1c597
Thomas Mortensen <STUDENT\tmort15@math-js04.srv.aau.dk>
2019-05-03 15:47:59 +0200

    asd

--- rollingnig.R
+++ rollingnig.R
@@ -3,8 +3,8 @@ library(tidyverse) ; library(GeneralizedHyperbolic) ; library(parallel)
 
 
 rolling.nig <- function(data,window,refit.every,cl){
-  
-  number <- ceiling(length(data)/refit.every)-(window/refit.every)
+  #browser()
+  number <- ceiling(length(data)/refit.every)-refit.every+1
   intervals <- matrix(0,nrow = number,ncol = 2)
   intervals[1,] <- c(1,window)
   for(i in 2:(number)){
@@ -15,11 +15,11 @@ rolling.nig <- function(data,window,refit.every,cl){
     library(GeneralizedHyperbolic)
     start <- nigFitStart(data[x[1]:x[2]],startValues = "MoM",startMethodMoM = "Nelder-Mead")
     fit <- try(nigFit(data[x[1]:x[2]], paramStart = start,method = "Nelder-Mead"))
-    if("try-error" %in% class(fit)) par <- start else{par <- as.numeric(fit$param)}
+    if("try-error" %in% class(fit)){par <- start}else{par <- as.numeric(fit$param)}
     return(par)
   }
   
-  
+  browser()
   parameters <- parApply(cl,intervals,MARGIN = 1,FUN = fitting) %>% t()
   
   stopCluster(cl)
@@ -27,8 +27,9 @@ rolling.nig <- function(data,window,refit.every,cl){
   return(parameters)
 }
 
-cl <- makePSOCKcluster(4)
-tic();fits <- rolling.nig(x,w,refit,cl);toc()
+
+cl <- makePSOCKcluster(10)
+fits <- rolling.nig(BAC.returns$Return[1:3910],391,10,cl)
 
 
 
