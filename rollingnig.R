63cf4fbc4ac08981c25eb22c2014d3bd8ed791b6
Thomas Mortensen <STUDENT\tmort15@math-js04.srv.aau.dk>
2019-05-06 14:30:49 +0200

    commit

--- rollingnig.R
+++ rollingnig.R
@@ -15,17 +15,28 @@ rolling.nig <- function(data,window,refit.every,cl){
   
   fitting <- function(x){
     library(GeneralizedHyperbolic)
-    start <- nigFitStart(data[x[1]:x[2]],startValues = "MoM",startMethodMoM = "Nelder-Mead")
+    start <- try(nigFitStart(data[x[1]:x[2]],startValues = "MoM",startMethodMoM = "Nelder-Mead"))
+    if("try-error" %in% class(start)){return("Error in MoM")}
     fit <- try(nigFit(data[x[1]:x[2]], paramStart = start,method = "Nelder-Mead"))
-    if(class(fit) == "try-error"){par <- start}else{par <- as.numeric(fit$param)}
+    if("try-error" %in% class(fit)){par <- start}else{par <- as.numeric(fit$param)}
     return(par)
   }
   
   
-  parameters <- parApply(cl,intervals,MARGIN = 1,FUN = fitting) %>% t()
+  parameters <- parApply(cl,intervals,MARGIN = 1,FUN = fitting)
   stopCluster(cl)
   
-  browser()
+  if(class(parameters) == "list"){
+    parameters <- do.call(rbind,parameters)
+    parameters <- parameters[,1:4]
+  }
+  
+  if(dim(parameters)[2]>4 & dim(parameters)[1]<=4){
+    parameters <- t(parameters)
+  }else if(dim(parameters)[2]>4 & dim(parameters)[1]>4){
+    return("Wrong parameter dimensions")
+  }
+  
   times <- Time[intervals[,2]+1]
   result <- data.frame(times,parameters);colnames(result) <- c("Time","mu","delta","alpha","beta")
   
@@ -39,8 +50,8 @@ rolling.nig <- function(data,window,refit.every,cl){
 
 library(tictoc)
 cl <- makePSOCKcluster(20)
-tic();fits <- rolling.nig(BAC.returns,5*391,30,cl);toc()
-save(fits,file = "BAC_NiG_forecasts.Rdata")
+tic();fits <- rolling.nig(WFC.returns,5*391,30,cl);toc()
+save(fits,file = "WFC_NiG_forecasts.Rdata")
 
 
 
