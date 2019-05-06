27dd9b8fa1ec721a82b94f1bc67ab075c458714d
Thomas Mortensen <39585003+Manatee1@users.noreply.github.com>
2019-05-06 09:05:26 +0200

    Update rollingnig.R

--- rollingnig.R
+++ rollingnig.R
@@ -1,10 +1,12 @@
 library(tidyverse) ; library(GeneralizedHyperbolic) ; library(parallel)
-
+load("Clean_data.RData")
 
 
 rolling.nig <- function(data,window,refit.every,cl){
+
+  Time <- data$Time ; data <- data$Return
   #browser()
-  number <- ceiling(length(data)/refit.every)-refit.every+1
+  number <- ceiling((length(data)-window)/refit.every)
   intervals <- matrix(0,nrow = number,ncol = 2)
   intervals[1,] <- c(1,window)
   for(i in 2:(number)){
@@ -15,22 +17,30 @@ rolling.nig <- function(data,window,refit.every,cl){
     library(GeneralizedHyperbolic)
     start <- nigFitStart(data[x[1]:x[2]],startValues = "MoM",startMethodMoM = "Nelder-Mead")
     fit <- try(nigFit(data[x[1]:x[2]], paramStart = start,method = "Nelder-Mead"))
-    if("try-error" %in% class(fit)){par <- start}else{par <- as.numeric(fit$param)}
+    if(class(fit) == "try-error"){par <- start}else{par <- as.numeric(fit$param)}
     return(par)
   }
   
-  browser()
+  #browser()
   parameters <- parApply(cl,intervals,MARGIN = 1,FUN = fitting) %>% t()
-  
   stopCluster(cl)
   
-  return(parameters)
+
+  times <- Time[intervals[,2]+1]
+  result <- data.frame(times,parameters);colnames(result) <- c("Time","mu","delta","alpha","beta")
+  
+  
+  return(result)
 }
 
 
-cl <- makePSOCKcluster(10)
-fits <- rolling.nig(BAC.returns$Return[1:3910],391,10,cl)
 
+# x <- BAC.returns[1:(10*391),]; w <- 5*391;refit.every <- 391
+
+
+cl <- makePSOCKcluster(4)
+tic();fits <- rolling.nig(BAC.returns[1:3910,],5*391,60,cl);toc()
+save(fits,file = "Rolling_forecasts.Rdata")
 
 
 
