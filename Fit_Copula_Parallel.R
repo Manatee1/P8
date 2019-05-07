4a9a39f138d7e339efeb83e2509b498aa17089ff
Toke Zinn <34207343+TokeZinn@users.noreply.github.com>
2019-05-07 15:24:39 +0200

    Fit Failed

--- Fit_Copula_Parallel.R
+++ Fit_Copula_Parallel.R
@@ -96,3 +96,76 @@ stopCluster(cl)
 
 save(Cop_est,file = "Estimated_Copula.RData")
 
+
+# Fit Failed --------------------------------------------------------------
+
+load("Estimated_Copula.RData")
+
+failed = rep(0, length(Cop_est))
+for(i in 1:length(Cop_est)){if("try-error" %in% class(Cop_est[[i]])){failed[i] = 1}}
+sum(failed)
+plot(failed)
+
+index = 1:length(Cop_est)
+failed_index = which(failed == 1) 
+
+Interval = Interval_Matrix(BAC.returns,5*391,30)
+
+Fit_Interval = function(i, r_1 = BAC.returns, r_2 = WFC.returns, f_1 = BAC_fit, f_2 = WFC_fit, cop = tCopula, Int = Interval){
+  browser()
+  library(GeneralizedHyperbolic);library(copula);
+  I = Int[i,]
+  
+  start = as.character(r_1$Time[I[1]]) 
+  end = as.character(r_1$Time[I[2]])
+  
+  X = r_1[I[1]:I[2],2]
+  Y = r_2[I[1]:I[2],2]
+  
+  p_1 = unlist(f_1[i,2:5])
+  p_2 = unlist(f_2[i,2:5])
+  
+  U = try(pnig(X, param = p_1))
+  
+  if("try-error" %in% class(U)){
+    f = ecdf(X)
+    U = rep(0,length(X))
+    for(i in 1:length(X)){
+      temp = try(pnig(X[i], param = p_1)) 
+      
+      if("try-error" %in% class(temp)){
+        U[i] = f(X[i])
+      }else{
+        U[i] = temp
+      }
+    }
+  }
+  
+  V = as.numeric(try(pnig(Y, param = p_2)))
+  
+  if("try-error" %in% class(V)){
+    f = ecdf(Y)
+    V = rep(0,length(Y))
+    for(i in 1:length(Y)){
+      temp = try(pnig(Y[i], param = p_2)) 
+      
+      if("try-error" %in% class(temp)){
+        V[i] = f(Y[i])
+      }else{
+        V[i] = temp
+      }
+    }
+  }
+  
+  W = cbind(U,V)
+  
+  C = cop(dim = 2)
+  fit = suppressWarnings(fitCopula(C,W))
+  
+  theta = fit@copula@parameters
+  
+  return(c(start,end,theta))
+}
+
+sapply(failed_index,Fit_Interval)
+
